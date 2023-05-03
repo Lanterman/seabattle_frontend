1e2b233134ed42f1a61f4b2d50e148379c5f2648
Lanterman <splen1011@mail.ru>
2023-05-03 20:22:24 +0300

    created a modal window and used it in the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,10 @@ import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShip } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
-import { sendDetermineWinner, sendCountDownTimer } from "../../modules/wsCommunication/wsLobby/wsLobbyRequests"
 import { clearState } from "../../store/reducers/lobbyReducer";
+import { ModalWindow } from "../ModalWindow/ModalWindow";
 
 import "./Header.css";
 
@@ -16,26 +17,20 @@ function Header(props) {
     const winner = useSelector(state => state.lobby.winner);
     const enemyBoard = useSelector(state => state.lobby.enemyBoard);
     const users = useSelector(state => state.lobby.users);
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [content, setContent] = useState({user_id: enemyBoard?.user_id})    
 
-    function beforeClosingPage(url) {
-        props.client.close();
-        props.setClient(null);
-        dispatch(clearState());
-        navigate(url);
-    };
-    
     function onClickHandler(e, url) {
         e.preventDefault();
         if (location.pathname.length >= 45) {
             if (!winner && users.length === 2 && enemyBoard.user_id) {
-                if (window.confirm("Do you really want to follow the link? \nIt will count as a loss!")) {
-                    sendDetermineWinner(props.client, enemyBoard.user_id);
-                    beforeClosingPage(url);
-                } else {
-                    sendCountDownTimer(props.client);
-                };
+                setContent(Object.assign(content, {url: url}));
+                setIsOpenModal(true);
             } else {
-                beforeClosingPage(url);
+                props.client.close();
+                props.setClient(null);
+                dispatch(clearState());
+                navigate(url);
             };
         } else {
             navigate(url);
@@ -55,6 +50,15 @@ function Header(props) {
                 <Link to="/" onClick={(e) => onClickHandler(e, "/")}>v0.1.0</Link>
                 <Link to="/" onClick={(e) => onClickHandler(e, "/")}>GitHub</Link>
             </div>
+
+            {isOpenModal && <ModalWindow 
+                                type="follow-link" 
+                                msg="Do you really want to follow the link? It will count as a loss!"
+                                client={props.client}
+                                setClient={props.setClient}
+                                setIsOpenModal={setIsOpenModal}
+                                content={content}/>}
+
         </header>
     );
 };  
--- src/components/ModalWindow/ModalWindow.js
+++ src/components/ModalWindow/ModalWindow.js
@@ -1,13 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose, faQuestion } from '@fortawesome/free-solid-svg-icons';
+
+import { sendDetermineWinner } from "../../modules/wsCommunication/wsLobby/wsLobbyRequests";
+import { clearState } from "../../store/reducers/lobbyReducer";
+
+
 import "./ModalWindow.css";
 
 
 function ModalWindow(props) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    function modalAgreeHandler() {
+        sendDetermineWinner(props.client, props.content.user_id);
+        props.client.close();
+        props.setClient(null);
+        dispatch(clearState());
+        props.setIsOpenModal(false);
+        navigate(props.content.url);
+    };
+
+    function modalCloseHandler() {
+        props.setIsOpenModal(false);
+    };
 
     return (
-        <div class="wrap">
-            <div class="modal">
-                <input type="button" onClick={() => {props.setIsOpenModal(false)}} value="close"/>
-                Modal
+        <div className="wrap">
+            <div className="modal">
+                <div className="modal-title">
+                    <FontAwesomeIcon icon={faQuestion} className="question-icon"/>
+                    <FontAwesomeIcon icon={faClose} className="close-icon" onClick={() => modalCloseHandler()} />
+                    <p className="title">Confirm action</p>
+                </div>
+                <p className="msg">{props.msg}</p>
+                <div className="buttons">
+                    <input type="button" className="back" value="Back" onClick={() => modalCloseHandler()} />
+                    <input type="button" className="agree" value="Ok" onClick={() => modalAgreeHandler()}/>
+                </div>
             </div>
         </div>
     );
