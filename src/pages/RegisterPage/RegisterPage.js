939abb1c3ce9fa2f0e169382a34c140076c5938e
Lanterman <splen1011@mail.ru>
2023-06-29 00:55:00 +0300

    Upgraded the registration page, registerUserAction depends on the type of action, if 'create' is a request to create a user, if 'activate' is a request to activate a user. Added user activation.

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,18 +1,22 @@
 import axios from "axios";
-import { Link, Form, redirect, useActionData } from "react-router-dom";
+import { Link, Form, useActionData, useNavigation, Navigate } from "react-router-dom";
 
 import "./RegisterPage.css";
 
 
 function RegisterPage(props) {
-    const errors = useActionData();
-
-    return (
+    const actionData = useActionData();
+    const navigation = useNavigation();
+    const token = sessionStorage.getItem("auth_token");
+    const isProcessing = ["submitting", "loading"].includes(navigation.state);
+    console.log(actionData)
+    return !token ? (
         <div className="main-page">
             <div className="register-page">
                 <p className="register-title">Sign up</p>
                 <Form className="sign-up" method="post">
-                    {errors?.non_field_errors && errors.non_field_errors.map((error, number) => {
+                    <input name="type" readOnly value="create" type="hidden" />
+                    {actionData?.errors?.non_field_errors && actionData?.errors.non_field_errors.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
@@ -20,7 +24,7 @@ function RegisterPage(props) {
                     <input placeholder="Username" required type="text" className="value"
                         name="username" minLength="5" maxLength="30" autoComplete="off"
                     />
-                    {errors?.username && errors.username.map((error, number) => {
+                    {actionData?.errors?.username && actionData?.errors.username.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
@@ -29,7 +33,7 @@ function RegisterPage(props) {
                     <input placeholder="First name" required type="text" className="value"
                         name="firstName" minLength="5" maxLength="30"
                     />
-                    {errors?.first_name && errors.first_name.map((error, number) => {
+                    {actionData?.errors?.first_name && actionData?.errors.first_name.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
@@ -38,7 +42,7 @@ function RegisterPage(props) {
                     <input placeholder="Last name" required type="text" className="value"
                         name="lastName" minLength="5" maxLength="30"
                     />
-                    {errors?.last_name && errors.last_name.map((error, number) => {
+                    {actionData?.errors?.last_name && actionData?.errors.last_name.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
@@ -47,7 +51,7 @@ function RegisterPage(props) {
                     <input placeholder="Email" required type="email" className="value"
                         name="email" minLength="5" maxLength="30" autoComplete="off"
                     />
-                    {errors?.email && errors.email.map((error, number) => {
+                    {actionData?.errors?.email && actionData?.errors.email.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
@@ -56,7 +60,7 @@ function RegisterPage(props) {
                     <input placeholder="Mobile number" required type="number" className="value"
                         name="mobileNumber" minLength="12" maxLength="20"
                     />
-                    {errors?.mobile_number && errors.mobile_number.map((error, number) => {
+                    {actionData?.errors?.mobile_number && actionData?.errors.mobile_number.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
@@ -65,7 +69,7 @@ function RegisterPage(props) {
                     <input placeholder="Password" required type="password" className="value"
                         name="password1"
                     />
-                    {errors?.password1 && errors.password1.map((error, number) => {
+                    {actionData?.errors?.password1 && actionData?.errors.password1.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
@@ -74,13 +78,13 @@ function RegisterPage(props) {
                     <input placeholder="Confirm password" required type="password" className="value"
                         name="password2"
                     />
-                    {errors?.password2 && errors.password2.map((error, number) => {
+                    {actionData?.errors?.password2 && actionData?.errors.password2.map((error, number) => {
                         return (<li key={number} className="error" >
                             {error}
                         </li>)
                     })}
 
-                    <input type="submit" value="Sign up" className="submit-register"/>
+                    <input type="submit" value="Sign up" className="submit-register" disabled={isProcessing}/>
                 </Form>
 
                 <div className="separation"></div>
@@ -91,7 +95,32 @@ function RegisterPage(props) {
                 </p>
             </div>
         </div>
-    );
+        ) :
+        (<div className="main-page">
+            <p className="info-to-activate">
+                You have been sent an email with a link to verify your account. Please follow it.
+            </p>
+            <Form className="form" method="post">
+                <input name="type" readOnly type="hidden" value="activate"/>
+                <div className="block">
+                    <span className="label">Verification code: </span>
+                    <input className={`value ${actionData?.errors?.detail ? "er-value" : ""}`} name="secretKey" 
+                        required autoComplete="off"
+                    />
+                </div>
+
+                {actionData?.errors?.detail && (
+                    <li className="activate-error" >
+                        {actionData?.errors?.detail}
+                    </li>
+                )}
+
+                <div className="button">
+                    <input type="submit" className="submit" value="Send" disabled={props.isProcessing} />
+                </div>
+            </Form>      
+            {actionData?.data?.detail === "is activated." && <Navigate to="/" />}         
+        </div>);
 };
 
 
@@ -103,29 +132,50 @@ async function registerUser(formData) {
             sessionStorage.setItem("username", formData.username);
             sessionStorage.setItem("refresh_token", response.data.refresh_token);
             // document.cookie = `refresh_token = ${response.data.refresh_token}`;
-            return redirect(`/profile/${formData.username}/`);
             })
             .catch((function(response) {
-              return response.response.data
+              return {errors: response.response.data};
           }));
 
-    return response
+    return response;
+};
+
+
+async function activateAccount(secretKey) {
+    const user_id = sessionStorage.getItem("user_id");
+
+    const response = await axios.get(`/auth/activate_account/${user_id}/${secretKey}/`)
+        .then(function(response) {
+           return {data: response.data}
+            })
+            .catch((function(response) {
+              return {errors: response.response.data};
+          }));
+
+    return response;
 };
 
 
 async function registerUserAction({request}) {
     const formData = await request.formData();
-    const inputData = {
-        username: formData.get("username"),
-        first_name: formData.get("firstName"),
-        last_name: formData.get("lastName"),
-        email: formData.get("email"),
-        mobile_number: formData.get("mobileNumber"),
-        password1: formData.get("password1"),
-        password2: formData.get("password2"),
+    let response = {};
+
+    if (formData.get("type") === "create") {
+        const inputData = {
+            username: formData.get("username"),
+            first_name: formData.get("firstName"),
+            last_name: formData.get("lastName"),
+            email: formData.get("email"),
+            mobile_number: formData.get("mobileNumber"),
+            password1: formData.get("password1"),
+            password2: formData.get("password2"),
+        };
+        response = await registerUser(inputData);
+
+    } else if (formData.get("type") === "activate") {
+        response = await activateAccount(formData.get("secretKey"));
     };
 
-    const response = await registerUser(inputData);
     return response;
 };
 
